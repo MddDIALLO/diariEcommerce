513fe06046ee95bd246366f8519952264e165887
MddDIALLO <122102997+MddDIALLO@users.noreply.github.com>
2023-07-20 10:53:40 +0200

    removing gmail account

--- src/loumodiari/settings.py
+++ src/loumodiari/settings.py
@@ -148,6 +148,6 @@ MESSAGE_TAGS = {
 # SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'diaridigital2023@gmail.com'
-EMAIL_HOST_PASSWORD = 'Diaridigital2023$'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
